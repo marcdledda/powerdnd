7afceec472eb3879a8a477195c667977c10e3874
marcdledda <marcdl636@gmail.com>
2018-07-13 02:22:47 -0500

    edit power database

--- src/components/MakePower.js
+++ src/components/MakePower.js
@@ -3,27 +3,14 @@ import PowerList from '../powers.json';
 
 class MakePower extends Component {
 
-    callPower = () => {
-        console.log("TEST");
-        console.log(PowerList.powers);
-    }
-
-    createPower = () => {
-        let data = {
-            "power": "haha",
-            "desc": "hehe",
-            "property": "hellye"
-        }
+    componentDidMount(){
+        console.log(PowerList);
     }
     
     render(){
         return(
           <div>
-              <input type="text" placeholder="Power Name"/> <br/>
-              <textarea placeholder="Description" cols="30" rows="10"></textarea> <br/>
-              <input type="text" placeholder="Property"/> <br/>
-              <button onClick={this.createPower}>Create Power</button> <br/>
-              <button onClick={this.callPower}>Check Powers</button>
+              TEST
           </div>  
         );
     }
